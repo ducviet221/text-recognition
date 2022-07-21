287e09d13ddf417c514234c321742d7d08716b44
hdtlgl301219 <hductung177@gmail.com>
2022-07-21 23:32:52 +0700

    update nhanh vietocr-training

--- tdrg/generators/__init__.py
+++
@@ -1,4 +0,0 @@
-from generators.from_dict import GeneratorFromDict
-from generators.from_random import GeneratorFromRandom
-from generators.from_strings import GeneratorFromStrings
-from generators.from_wikipedia import GeneratorFromWikipedia
