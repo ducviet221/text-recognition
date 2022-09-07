3692b1a6a0576f17a5cd465b02a8d6c62a727d22
ahri22 <thithutrang2211@gmail.com>
2022-09-07 16:31:04 +0700

    final

--- script.py
+++ script.py
@@ -7,49 +7,15 @@ import imgaug
 #segmentation maps in a variety of different ways.
 import numpy as np
 import matplotlib.pyplot as plt
+from keras_ocr import datasets
 import sklearn.model_selection
 import tensorflow as tf
 import typing
-import kerasocr
+import keras_ocr
+from keras_ocr import datasets
 
 data_dir = '.'
-
-def get_dataset(training_gt_dir,training_images_dir,skip_illegible=False):
-  dataset=[]
-  image_files_name = os.listdir(training_images_dir)
-  label_files_name = os.listdir(training_gt_dir)
-  for image_file, label_file in zip(image_files_name,label_files_name):
-        image_path = os.path.join(training_images_dir, image_file )
-        lines = []
-        with open(os.path.join(training_gt_dir, label_file), "r", encoding="utf8") as f:
-            current_line: typing.List[typing.Tuple[np.ndarray, str]] = []
-            for raw_row in f.read().split("\n"):
-                if raw_row == "":
-                    lines.append(current_line)
-                    current_line = []
-                else:
-                    row = raw_row.split(" ")[4:]
-                    #take from the 4th element onwards
-                    character = row[-1][1:-1]
-                    #the last element is the letter
-                    if character == "" and skip_illegible:
-                        continue
-                    x1, y1, x2, y2 = map(float, row[:4])
-                    #get 4 final coordinates of file label execpt last letter
-                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                    #cast type to int
-                    current_line.append(
-                        (np.array([[x1, y1], [x2, y1], [x2, y2], [x1, y2]]), character)
-                    )
-        lines = [line for line in lines if line]
-        dataset.append((image_path, lines, 1))
-  return dataset
-
-training_gt_dir="D:/text-recognition\dataset\labels"
-training_images_dir="D:/text-recognition\dataset\images"
-
-dataset=get_dataset(training_gt_dir,training_images_dir,skip_illegible=False)
-
+dataset = keras_ocr.datasets.dataset 
 train, validation = sklearn.model_selection.train_test_split(
     dataset, train_size=0.8, random_state=42
 )
@@ -68,17 +34,17 @@ augmenter = imgaug.augmenters.Sequential([
     # which can end up changing the color of the images.
 ])
 generator_kwargs = {'width': 640, 'height': 640}
-training_image_generator = kerasocr.datasets.get_detector_image_generator(
+training_image_generator = keras_ocr.datasets.get_detector_image_generator(
     labels=train,
     augmenter=augmenter,
     **generator_kwargs
 )
-validation_image_generator = kerasocr.datasets.get_detector_image_generator(
+validation_image_generator = keras_ocr.datasets.get_detector_image_generator(
     labels=validation,
     **generator_kwargs
 )
 
-detector = kerasocr.detection.Detector()
+detector = keras_ocr.detection.Detector()
 
 batch_size = 1
 training_generator, validation_generator = [
@@ -96,9 +62,9 @@ detector.model.fit_generator(
         tf.keras.callbacks.EarlyStopping(restore_best_weights=True, patience=5),
         #patince: number of epochs with no improvement after which training will be stopped
         #the loss for 5 consecutive epochs.
-        tf.keras.callbacks.CSVLogger(os.path.join(data_dir, 'detector_icdar2013.csv')),
+        tf.keras.callbacks.CSVLogger(os.path.join(data_dir, 'detector_keras_ocr.csv')),
         #Callback that streams epoch results to a CSV file.
-        tf.keras.callbacks.ModelCheckpoint(filepath=os.path.join(data_dir, 'D:/text-recognition/detector_icdar2013.h5'))
+        tf.keras.callbacks.ModelCheckpoint(filepath=os.path.join(data_dir, 'D:/text-recognition/detector_keras_vinAI'))
         #Callback to save the Keras model or model weights at some frequency.
     ],
     validation_data=validation_generator,
